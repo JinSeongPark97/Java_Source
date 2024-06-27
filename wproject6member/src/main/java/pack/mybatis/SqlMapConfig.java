6dfab98a947811011d8b0cafc17668efb7618242
jinseong <pjs19970@naver.com>
2024-06-27 18:19:40 +0900

    20240627 study

--- wproject6member/src/main/java/pack/mybatis/SqlMapConfig.java
+++ wproject6member/src/main/java/pack/mybatis/SqlMapConfig.java
@@ -6,20 +6,20 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class SqlMapConfig {
-	 public static SqlSessionFactory sqlSession;  //DB의 SQL명령을 실행시킬 때 필요한 메소드를 갖고 있다.
-	 
-	  static{
-	     String resource = "pack/mybatis/Configuration.xml"; // XML파일 읽기
-	     try {
-	         Reader reader = Resources.getResourceAsReader(resource); // configuration.xml을 읽음
-	         sqlSession = new SqlSessionFactoryBuilder().build(reader); // 객체 생성
-	         reader.close();
-	     } catch (Exception e) {
-	     System.out.println("SqlMapConfig 오류 : " + e);
-	  }
+	public static SqlSessionFactory sqlSession; // DB의 SQL명령을 실행시킬 때 필요한 메소드를 갖고 있다.
+
+	static {
+		String resource = "pack/mybatis/Configuration.xml";
+		try {
+			Reader reader = Resources.getResourceAsReader(resource);   
+			sqlSession = new SqlSessionFactoryBuilder().build(reader); 
+			reader.close();
+		} catch (Exception e) {
+			System.out.println("SqlMapConfig 오류 : " + e);
+		}
 	}
-	 
-	public static SqlSessionFactory getSqlSession(){  
-	     return sqlSession;
-	  }
+
+	public static SqlSessionFactory getSqlSession() {
+		return sqlSession;
 	}
+}
