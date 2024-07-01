8633d159672c154705079c982f16aafdacdae29a
jinseong <pjs19970@naver.com>
2024-07-01 18:09:31 +0900

    20240701 MVC

---
+++ wmvc4sangpum/src/main/java/pack/controller/CommandInter.java
@@ -0,0 +1,10 @@
+package pack.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface CommandInter {
+	String showData(HttpServletRequest request, HttpServletResponse response) throws Exception; 
+	
+	CommandInter inter = null;
+}
